8cd1680f7f9a0d36983425ae090bc556fe95b885
Jancygit19 <jancydev1987@gmail.com>
2022-02-06 20:55:01 +0530

    task to folder

---
+++ src/org/git/GitTask1.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class GitTask1 {
+	public void add() {
+		System.out.println("add");
+	}
+	public static void main(String[] args) {
+		GitTask1 git = new GitTask1();
+		git.add();
+	}
+
+}
